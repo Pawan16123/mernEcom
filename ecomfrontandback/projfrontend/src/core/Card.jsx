0c9259a935c958fea23ad27094f1d19d10accc81
Pawan Poojary <poojary.pawan16@gmail.com>
2022-12-04 06:57:36 +0530

    Default images added

--- ecomfrontandback/projfrontend/src/core/Card.jsx
+++ ecomfrontandback/projfrontend/src/core/Card.jsx
@@ -14,7 +14,8 @@ function Card({product, addRemove = true, remount, setRemount, cartOrCard = true
         .then((el)=>{
             console.log('How to do this', el)
             if(el.ok){
-                setImgUrl(`/product/photo/${product._id}`)
+                // setImgUrl(`/product/photo/${product._id}`)
+                setImgUrl("https://c4.wallpaperflare.com/wallpaper/365/244/884/uchiha-itachi-naruto-shippuuden-anbu-silhouette-wallpaper-preview.jpg")
             }else{
                 setImgUrl("https://c4.wallpaperflare.com/wallpaper/365/244/884/uchiha-itachi-naruto-shippuuden-anbu-silhouette-wallpaper-preview.jpg")
             }
